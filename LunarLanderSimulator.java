c22f11a63c3079fcbd7b07c167a6d87a10cc8e22
lajsha02 <lisadahl@kth.se>
2025-09-25 12:13:00 +0200

    Update LunarLanderSimulator.java

--- LunarLanderSimulator.java
+++ LunarLanderSimulator.java
@@ -5,21 +5,19 @@ import lunarLanderPackage.*;
         String currentState; 
         // Implementerar LunarLanderListener
     /* getHeight() → aktuell höjd i meter
-
     getVelocity() → aktuell hastighet i m/s
-
     getFuel() → återstående bränsle i procent
-
     getState()*/
 
-        public void onShipUpdate(ShipEvent e) {
+        public void onShipUpdated(Shipevent e) {
             currentState = e.getState();
             System.out.println("Ship Height: " + e.getHeight() + 
                               "Ship Velocity: " + e.getVelocity() +
                                "Ship Fuel: " + e.getFuel() +
                                "Ship State: " + e.getState());
             if (!currentState.equals("hovering")) {
-                System.out.println("Ship State: " + e.getState());
+                System.out.println("Final State: " + e.getState());
+                System.exit(0);
             }
         }
 
@@ -27,13 +25,14 @@ import lunarLanderPackage.*;
         public static void main(String[] args) {
             System.out.println("Lunar Lander Simulator");      
             LunarLander lander = new LunarLander();
-            //shutdown = new shutDownEngine();
             LunarLanderSimulator simulator = new LunarLanderSimulator();
             lander.setListener(simulator);
             lander.run();
 
             Scanner input = new Scanner(System.in);
             System.out.println("Press P to start the engine and A to shut it down: ");
+            // Så länge som state är hovering så kan användaren styra
+
             while (simulator.currentState == null || simulator.currentState.equals("hovering")) {
                 String S = input.nextLine();
                 if (S.equals("P")) {
@@ -44,8 +43,6 @@ import lunarLanderPackage.*;
                     System.out.println("Engine Shutdown");
                 }
 
-
-                
             }
             input.close();
     }
